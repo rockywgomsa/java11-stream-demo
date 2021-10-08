8fe3bd2ea49f89bb82e2d574e856ebd4d764839a
rockywgomsa <wgomsa2018@gmail.com>
2021-10-08 18:58:39 -0500

    Se crea la clase App2 para ejemplos de lambdas

--- src/main/java/com/mitocode/App2.java
+++ src/main/java/com/mitocode/App2.java
@@ -1,7 +1,27 @@
 package com.mitocode;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App2 {
 
-	private String nombre;
-	private int edad;
+	public static void main(String[] args) {
+
+		List<Integer> listNumeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+//		listNumeros.forEach(System.out::println);
+	
+		List<Integer> lista = App2.obbtenerElementosMayorA2(listNumeros);
+		lista.forEach(System.out::println);
+
+	}
+	
+	public static List<Integer> obbtenerElementosMayorA2(List<Integer> list) {
+		List<Integer> lista = list.stream()
+				.filter(x -> x>2)
+				.collect(Collectors.toList());
+		
+		return lista;
+	}
+
 }
